1baf484c1b1bf56bb605be54780dfb47ecd1b50a
Admin <Admin@DESKTOP-UGP0C2G>
2024-10-17 21:55:56 +0200

    Добавьте файлы проекта.

---
+++ legkiyyorven/Program.cs
@@ -0,0 +1,24 @@
+﻿// 1)
+//try
+//{ 
+//    int x = 1;
+//    int y = 2;
+//    bool result = (x >= 0) ^ ((y * y) != 4);
+//    Console.WriteLine($"Выражение равно {result}");
+//}
+//catch
+//{
+
+//}
+// 2)
+//try
+//{
+//    int x = 2;
+//    int y = 1;
+//    bool result = (x * y != 0) || (y < x);
+//    Console.WriteLine($"Выражение равно {result}");
+//}
+//catch
+//{
+
+//}
